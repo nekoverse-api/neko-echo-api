0ba90099970ec29cffa6d11a5017a7f88427dfa5
Gary Ascuy <gary.ascuy@gmail.com>
2025-03-26 22:05:18 -0500

    Adding base files

---
+++ src/test/java/io/ziogd/GamesTest.java
@@ -0,0 +1,21 @@
+package io.ziogd;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class GamesTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
