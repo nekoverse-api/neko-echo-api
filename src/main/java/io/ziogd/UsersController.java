9916fd5997b125e4e451923ed2ceaeb88fa06eae
Gary Ascuy <gary.ascuy@gmail.com>
2025-03-27 08:19:22 -0500

    removing unnecessary values

--- src/main/java/io/ziogd/UsersController.java
+++ src/main/java/io/ziogd/UsersController.java
@@ -1,7 +1,5 @@
 package io.ziogd;
 
-import static io.micronaut.http.MediaType.APPLICATION_JSON;
-
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Delete;
@@ -14,19 +12,19 @@ import java.util.UUID;
 
 @Controller("/users")
 public class UsersController {
-    @Get(uri="/", produces=APPLICATION_JSON)
+    @Get(uri="/")
     public List<User> getAll() {
         final var user1 = new User(UUID.randomUUID().toString(), "Gary Ascuy", 22);
         final var user2 = new User(UUID.randomUUID().toString(), "Gory Yucsa", 66);
         return List.of(user1, user2);
     }
 
-    @Get(uri="/{sid}", produces=APPLICATION_JSON)
+    @Get(uri="/{sid}")
     public User getBy(final String sid) {
         return new User(sid, "Gory Yucsa", 66);
     }
 
-    @Post(uri="/", consumes=APPLICATION_JSON, produces=APPLICATION_JSON)
+    @Post(uri="/")
     public User create(@Body final User user) {
         final var sid = UUID.randomUUID().toString();
         return new User(sid, user.getName(), user.getAge());
