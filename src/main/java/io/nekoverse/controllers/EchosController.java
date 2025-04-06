e0873ba0d337857d4d26b45f1ca598ff6a2b68fe
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-05 20:30:52 -0500

    Adding parameters

--- src/main/java/io/nekoverse/controllers/EchosController.java
+++ src/main/java/io/nekoverse/controllers/EchosController.java
@@ -1,19 +1,26 @@
 package io.nekoverse.controllers;
 
-import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
 import io.nekoverse.echo.api.EchosApi;
 import io.nekoverse.echo.model.EchoResponse;
 import io.nekoverse.services.EchosService;
 import jakarta.inject.Inject;
-import org.jsoup.Connection;
+import java.util.Map;
 
 @Controller
 public class EchosController implements EchosApi {
   @Inject private EchosService echosService;
 
   @Override
-  public EchoResponse getBy(String path) {
+  public EchoResponse getBy(String path, Map<String, Object> args) {
+    return echosService.buildResponse(path);
+  }
+
+  // TODO: Update generator to add request parameter or queries
+  @Post("{path}")
+  public EchoResponse postBy(String path, HttpRequest<?> req) {
     return echosService.buildResponse(path);
   }
 }
